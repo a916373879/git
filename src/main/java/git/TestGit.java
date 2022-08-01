7e3f444197bcd530a901c2358395195ab2cb77a1
yc <916373879@qq.com>
2022-08-01 19:14:32 +0800

    first commit

--- src/main/java/git/TestGit.java
+++ src/main/java/git/TestGit.java
@@ -3,5 +3,17 @@ package git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
     }
 }
